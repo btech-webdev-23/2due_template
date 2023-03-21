b4348296131c4a823949d38f81ed2f5b41d9aa45
RATIU5 <jcmemmott20@outlook.com>
2023-03-21 14:10:54 -0600

    added clarifying comments

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+//* Select the down arrow button and add a click event listener.
+//* Call a function called scrollTo that takes in an element as a parameter that it will scroll to.
+
+// add code here
+
+//* Create a function called scrollTo that takes in an element as a parameter.
+//* Select the element from the DOM and add the scrollIntoView method to it.
+//* Pass an object into the scrollIntoView method with the block property set
+//* to "start" and the behavior property set to "smooth".
+
+// add code here
+
+//* Select the alternate text element with jQuery.
+//* Change the color of the text to a color of your choice.
+
+// add code here
+
+//* Create two variables called count and wordsArray.
+//* Set count to 0 and wordsArray to an array of strings, which contain the words you want to cycle through.
+//* Create a setInterval function that takes in a callback function and a number.
+//* The callback function will be named changeText and will take in no parameters.
+//* Set the number to 5000, which will be the number of milliseconds between each change.
+
+// add code here
+
+//* Create a function called changeText. Inside, the count should increment by 1.
+//* Using the altText variable you created earlier, use the fadeOut method to fade out the text.
+//* The fadeout method takes in a number as a parameter, which is the number of milliseconds it will take to fade out. The second parameter is a callback function that will be called when the fadeOut method is complete.
+//* The callback function will use the text method to change the text to the next word in the wordsArray. You can achieve this using the .text method and the modulo operator. The modulo operator will return the remainder of the count divided by the length of the wordsArray. This will allow you to cycle through the wordsArray.
+//* Fade in the text using the jQuery fadeIn method. The fadeIn method takes in a number as a parameter, which is the number of milliseconds it will take to fade in.
+
+// add code here
---
+++ scripts/todo.js
@@ -0,0 +1,97 @@
+//* Select the todo input and add a focus event listener.
+//* When the input is focused, add a box shadow to the input.
+
+// add code here
+
+//* Select the todo input and add a focusout event listener.
+//* When the input is not focused, remove the box shadow. You can do that by adding a focusout event listener.
+
+// add code here
+
+//* Create a function called noMoreTodos. Inside the function, select the todo list (ul) and store it in a variable called todoList. Create a p element and store it in a variable called noTodos. Add the text "No todos!" to the noTodos element. Prepend the noTodos element to the todoList element.
+
+// add code here
+
+//* Create another function called removeNoTodos. Inside the function, select the p element with the text "No todos!" and remove it from the DOM, only if it exists. If it doesn't exist, do nothing.
+
+// add code here
+
+//* Outside all functions, select the add button and add a click event listener.
+
+// add code here
+
+//! Inside the callback function for the click event listener on the add button.
+
+//* Within the click event listener callback function, create a variable called todoText and set it to the value of the todo input.
+
+// add code here
+
+//* Trim the todoText variable to remove any whitespace.
+
+// add code here
+
+//* If the todoText value is empty, alert an error message and return out of the function.
+
+// add code here
+
+//* Select the item list (ul)
+
+// add code here
+
+//* Create a new li element and store it in a variable called newTodo.
+
+// add code here
+
+//* Create a new button element called todoButton.
+
+// add code here
+
+//* Add the class "todo-item-button" to the todoButton element.
+
+// add code here
+
+//* Add the text from the input into a new text node and append it to the todoButton element.
+
+// add code here
+
+//* Add a click event listener to the todoButton element.
+
+// add code here
+
+//* Within the click event listener callback function, change the style of the todoButton element to toggle a class called "strike".
+
+// add code here
+
+//* Create a new button element called todoDelete.
+
+// add code here
+
+//* Add the class "todo-item-button-delete" to the todoDelete element.
+
+// add code here
+
+//* Add a click event listener to the todoDelete element.
+
+// add code here
+
+//* Within the click event listener callback function, remove the whole newTodo element (li) from the DOM.
+
+// add code here
+
+//* Add the text "x" into a new text node and append it to the todoDelete element.
+
+// add code here
+
+//* Append the todoButton and todoDelete elements to the newTodo element.
+
+// add code here
+
+//* Prepend the newTodo element to the previously selected item list (ul).
+
+// add code here
+
+//* Clear the input value from the text box.
+
+// add code here
+
+//! End of callback function for the click event listener on the add button.
